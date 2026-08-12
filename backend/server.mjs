8828690f97878ec4ad4ee46337678fa946388b6e
Ayyan Rizwan <ayyan786920@gmail.com>
2026-08-12 15:24:10 +0500

    code

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,14 +1,19 @@
 import express from "express"
+import cors from "cors"
 import { postRouter } from "./routes/index.mjs"
 
 const app = express()
 const port = 3000
 
+app.use(cors())          
 app.use(express.json())
 
-app.get("/", (req, res, next) => {
-    res.send("hello world")})
+app.get("/", (req, res) => {
+  res.send("hello world")
+})
 
 app.use("/api/v1", postRouter)
 
-app.listen(port, () => console.log(`server is running...${port}`))
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+})
